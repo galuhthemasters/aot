07ab0870b95b000bd3d3d180d4ff46b2adefe313
vm-ukraine <33925688+vm-ukraine@users.noreply.github.com>
2017-11-28 23:36:46 +0200

    Add files via upload

--- install.bat
+++ install.bat
@@ -1,3 +1,5 @@
-:: Change HKCU to HKLM if you want to install globally.
-:: %~dp0 is the directory containing this bat script and ends with a backslash.
+ECHO { "name": "always_on_top", "description": "Always on Top", "path": "AOT.exe", "type": "stdio", "allowed_origins": [ "chrome-extension://dikebkpbfccagdgapbaecckeiaggejbh/" ] } > aot_manifest_ch.json
+REG ADD "HKCU\Software\Google\Chrome\NativeMessagingHosts\always_on_top" /ve /t REG_SZ /d "%~dp0aot_manifest_ch.json" /f
+
+ECHO { "name": "always_on_top", "description": "Always on Top", "path": "AOT.exe", "type": "stdio", "allowed_extensions": [ "{E6C93316-271E-4b3d-8D7E-FE11B4350AEB}" ] } > aot_manifest.json
 REG ADD "HKCU\Software\Mozilla\NativeMessagingHosts\always_on_top" /ve /t REG_SZ /d "%~dp0aot_manifest.json" /f
